3b68dedb1bb1f994dde5d3cf2d0f9cc52c992748
Xiaotian Lu <rricolu@gmail.com>
2020-04-10 02:00:29 +1000

    Updates

---
+++ precache-manifest.81ae74472a5303d05474d09ee6a53aa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e279ef3db4928339df7e5527d427c257",
+    "url": "/resume1/index.html"
+  },
+  {
+    "revision": "51bf716b9196edb1f909",
+    "url": "/resume1/static/css/main.cd4a5e57.chunk.css"
+  },
+  {
+    "revision": "f690445ea8bc069e6c7a",
+    "url": "/resume1/static/js/2.5de9998c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume1/static/js/2.5de9998c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51bf716b9196edb1f909",
+    "url": "/resume1/static/js/main.2a990d36.chunk.js"
+  },
+  {
+    "revision": "877c0e170ca2450a54d0",
+    "url": "/resume1/static/js/runtime-main.4f68255b.js"
+  },
+  {
+    "revision": "de15aaec8d7c8f2eb23a17db38bd1ab2",
+    "url": "/resume1/static/media/avatar.de15aaec.jpg"
+  }
+]);
